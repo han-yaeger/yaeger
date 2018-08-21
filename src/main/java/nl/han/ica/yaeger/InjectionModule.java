c7648897ea51b73e467d8cc76efb1c5934942dd3
Meron Brouwer <meron.brouwer@gmail.com>
2018-08-21 14:30:44 +0200

    [enhancement] #8 Added an Google Guice Module

---
+++ src/main/java/nl/han/ica/yaeger/InjectionModule.java
@@ -0,0 +1,14 @@
+package nl.han.ica.yaeger;
+
+import com.google.inject.AbstractModule;
+
+/**
+ * De InjectionModule is nodig voor het gebruik van Google Guice
+ */
+public class InjectionModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+
+    }
+}
