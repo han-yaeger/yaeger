a94ece6d71780eff119136ccd698628bcfda572c
Meron Brouwer <meron.brouwer@gmail.com>
2018-08-28 09:30:11 +0200

    [enhancement] Added a Unittest

--- src/test/java/nl/han/ica/yaeger/entities/EntityCollectionTest.java
+++ src/test/java/nl/han/ica/yaeger/entities/EntityCollectionTest.java
@@ -1,13 +1,18 @@
 package nl.han.ica.yaeger.entities;
 
+import javafx.beans.InvalidationListener;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.scene.Group;
+import javafx.scene.Node;
+import nl.han.ica.yaeger.entities.interfaces.Updatable;
 import org.junit.jupiter.api.Assertions;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 class EntityCollectionTest {
 
@@ -38,5 +43,40 @@ class EntityCollectionTest {
         // Verify
         Assertions.assertEquals(0, entityCollection.getNumberOfDynamicEntities());
         Assertions.assertEquals(0, entityCollection.getNumberOfStaticEntities());
+    }
+
+    @Test
+    void initWithDynamicEntitiesAreUpdatedWhenUpdateIsCalled() {
+        // Setup
+        UpdatableEntity updatableEntity = Mockito.mock(UpdatableEntity.class);
+        Node node = Mockito.mock(Node.class);
+        Mockito.when(updatableEntity.getGameNode()).thenReturn(node);
+
+        Set<Entity> dynamicSet = new HashSet<>();
+        dynamicSet.add(updatableEntity);
+
+        Group group = Mockito.mock(Group.class);
+        ObservableList<Node> children = Mockito.mock(ObservableList.class);
+        Mockito.when(group.getChildren()).thenReturn(children);
+
+        // Test
+        entityCollection = new EntityCollection(group, dynamicSet);
+        entityCollection.update();
+
+        // Verify
+        Mockito.verify(updatableEntity).update();
+    }
+}
+
+class UpdatableEntity extends Entity implements Updatable {
+
+    @Override
+    public Node getGameNode() {
+        return null;
+    }
+
+    @Override
+    public void update() {
+        // Not required here.
     }
 }
