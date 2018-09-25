07ade9f7fcfd4974212d36e9a81f96d88ab1e5ca
Meron Brouwer <meron.brouwer@gmail.com>
2018-09-25 13:50:46 +0200

    [translate] Added some more JavaDoc

--- src/main/java/nl/han/ica/yaeger/StatisticsObserver.java
+++ src/main/java/nl/han/ica/yaeger/StatisticsObserver.java
@@ -2,8 +2,18 @@ package nl.han.ica.yaeger;
 
 import nl.han.ica.yaeger.entities.EntityCollectionStatistics;
 
+/**
+ * A {@link StatisticsObserver} will function as the {@code Observer} from the Observable-pattern, for changes in
+ * the {@link EntityCollectionStatistics}.
+ */
 @FunctionalInterface
 public interface StatisticsObserver {
 
+    /**
+     * Is called by the observed {@link nl.han.ica.yaeger.entities.EntityCollection}.
+     *
+     * @param statistics An instance of {@link EntityCollectionStatistics} that encapsulates the latest statistical
+     *                   information regarding the observed {@link nl.han.ica.yaeger.entities.EntityCollection}.
+     */
     void update(EntityCollectionStatistics statistics);
 }
