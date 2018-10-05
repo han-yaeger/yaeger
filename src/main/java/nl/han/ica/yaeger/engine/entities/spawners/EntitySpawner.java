b64a66bf84025fa4d4d79563dc740279ee52858a
Meron Brouwer <meron.brouwer@gmail.com>
2018-10-05 13:20:38 +0200

    [refactor] Minor refactorings and added some Javadoc

--- src/main/java/nl/han/ica/yaeger/engine/entities/spawners/EntitySpawner.java
+++ src/main/java/nl/han/ica/yaeger/engine/entities/spawners/EntitySpawner.java
@@ -7,27 +7,41 @@ import nl.han.ica.yaeger.engine.entities.entity.Entity;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * An {@code EntitiySpawner} is the abstract superclass that thould be extended to create an object that
+ * spawns a subclass of {@link Entity}.
+ */
 public abstract class EntitySpawner implements Destructable {
 
     private Set<Entity> spawnedEntities = new HashSet<>();
-    private AnimationTimer animator;
+    private AnimationTimer timer;
 
     private long interval;
 
     /**
-     * Create a new EntitySpawner.
+     * Create a new {@code EntitySpawner}.
      *
-     * @param interval The interval in milli seconds
+     * @param interval the interval at which instances of {@link Entity} should ne spawned, in milli-seconds
      */
     public EntitySpawner(long interval) {
         this.interval = interval * 1000000;
         initTimer();
     }
 
+    /**
+     * Spawn an {@link Entity}.
+     *
+     * @param entity the {@link Entity} to be spawned
+     */
     protected void spawn(Entity entity) {
         spawnedEntities.add(entity);
     }
 
+    /**
+     * Return the {@link Set} of spawned {@code Entities}. After this method is called, the {@link Set} is cleared.
+     *
+     * @return a {@link Set} containing instances of {@link Entity d}
+     */
     public Set<Entity> getSpawnedEntities() {
         if (spawnedEntities.isEmpty()) {
             return new HashSet<>();
@@ -38,10 +52,18 @@ public abstract class EntitySpawner implements Destructable {
         }
     }
 
+    /**
+     * Called by the {@code EntitySpawner} every time a new {@link Entity} should be spawned.
+     */
+    public abstract void tick();
+
+    @Override
+    public void destroy() {
+        timer.stop();
+    }
+
     private void initTimer() {
-
-
-        animator = new AnimationTimer() {
+        timer = new AnimationTimer() {
 
             private long lastUpdate = System.currentTimeMillis();
 
@@ -55,13 +77,6 @@ public abstract class EntitySpawner implements Destructable {
             }
         };
 
-        animator.start();
-    }
-
-    public abstract void tick();
-
-    @Override
-    public void destroy() {
-        animator.stop();
+        timer.start();
     }
 }
