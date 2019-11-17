d99cb6f600555b5f7da387da20a81c43aedcae4f
Meron Brouwer <meron.brouwer@gmail.com>
2019-11-17 20:32:24 +0100

    [bugifx] Added all relevant information to the module-info.java that is needed to run the two games.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,18 @@ module nl.meron.yaeger {
 
     exports nl.meron.pong;
     exports nl.meron.waterworld;
+    exports nl.meron.yaeger.engine.entities;
+    exports nl.meron.yaeger.engine.scenes.impl;
+    exports nl.meron.yaeger.engine.scenes.delegates;
+    exports nl.meron.yaeger.engine.media.repositories;
+    exports nl.meron.yaeger.javafx.image;
+    exports nl.meron.yaeger.engine.debug;
+    exports nl.meron.yaeger.javafx.debug;
+    exports nl.meron.yaeger.guice.factories;
+    exports nl.meron.yaeger.javafx.animationtimer;
+    exports nl.meron.yaeger.engine.entities.entity.sprites;
+    exports nl.meron.yaeger.engine.entities.entity;
+
+    opens pong;
+    opens waterworld;
 }
