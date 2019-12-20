7e13ecdbb0c2ae84676fdaea79832a079ca0e41b
Meron Brouwer <meron.brouwer@gmail.com>
2019-12-20 11:39:49 +0100

    [enhancement] Added the missing test for Point

--- src/test/java/nl/meron/yaeger/engine/entities/entity/PointTest.java
+++ src/test/java/nl/meron/yaeger/engine/entities/entity/PointTest.java
@@ -1,5 +1,6 @@
 package nl.meron.yaeger.engine.entities.entity;
 
+import javafx.geometry.Point2D;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,10 +8,24 @@ import static org.junit.jupiter.api.Assertions.*;
 class PointTest {
 
     @Test
-    void addingPositionsWorksAsExpected() {
+    void addingPointWorksAsExpected() {
         // Setup
         var position = new Point(0d, 0d);
         var positionToAdd = new Point(1d, 1d);
+
+        // Test
+        var addedPosition = position.add(positionToAdd);
+
+        // Verify
+        assertEquals(1d, addedPosition.getX());
+        assertEquals(1d, addedPosition.getY());
+    }
+
+    @Test
+    void addingPoint2DWorksAsExpected() {
+        // Setup
+        var position = new Point(0d, 0d);
+        var positionToAdd = new Point2D(1d, 1d);
 
         // Test
         var addedPosition = position.add(positionToAdd);
