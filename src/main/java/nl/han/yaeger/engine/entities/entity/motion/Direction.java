8bff00b839da2e08c8e4620c061affc203efc5f8
Meron Brouwer <meron.brouwer@gmail.com>
2020-04-21 15:04:00 +0200

    [refactor] Minor change in JavaDoc

--- src/main/java/nl/han/yaeger/engine/entities/entity/motion/Direction.java
+++ src/main/java/nl/han/yaeger/engine/entities/entity/motion/Direction.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Convenience enum for setting the {@link Direction}
+ * Convenience enum for setting the {@link Direction}.
  */
 public enum Direction {
 
