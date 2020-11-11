03d68d6e29c4181d0beb6a2c65c4a57e3c772082
Meron Brouwer <meron.brouwer@gmail.com>
2020-11-11 20:30:33 +0100

    [documentation] Added JavaDoc to the module descriptor

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,7 @@
+/**
+ * The main (and only) module for Yaeger. Currently both the public and internal API are part of
+ * this one module. In the future they should be separated which will leave a API-module and a Core-module.
+ */
 module hanyaeger.api {
     requires transitive javafx.base;
     requires transitive javafx.controls;
