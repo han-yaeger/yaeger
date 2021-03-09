06657d3ba5124a0dddb37bff1af85f42c7ec5779
Meron Brouwer <meron.brouwer@gmail.com>
2021-03-09 20:16:01 +0100

    [enhancement] Added the methods to the interface

--- src/main/java/com/github/hanyaeger/api/engine/entities/entity/motion/MotionModifier.java
+++ src/main/java/com/github/hanyaeger/api/engine/entities/entity/motion/MotionModifier.java
@@ -46,6 +46,39 @@ public interface MotionModifier extends SpeedProvider, DirectionProvider {
     void addToMotion(final double speed, final Direction direction);
 
     /**
+     * @param direction the {@link Direction} in which the speed should be inverted
+     * @see #invertSpeedInDirection(double)
+     */
+    void invertSpeedInDirection(final Direction direction);
+
+    /**
+     * Since a motion can be described as a vector, such a vector can be decomposed in two perpendicular
+     * components, of which one is parallel to the given direction. Calling this methods inverts the vector
+     * parallel to the direction and computes a new motion, based on that vector.
+     *
+     * <p> TODO
+     * <img width="30%" src="doc-files/invert-motion-vectors.svg" alt="Vector representation of inverting the motion in a given direction">
+     * <p>
+     * TODO
+     * <p> In the image above, let <b>v</b> denote the vector associated with the current motion and <b>b</b> be the vector
+     * in the direction that has to be nullified, then the resulting vector <b>p</b> can be calculated by
+     * <p>
+     * \( \textbf{p} = \cfrac{\textbf{v}\cdot\textbf{b}}{\textbf{b}\cdot\textbf{b}} \cdot \textbf{b}   \)
+     *
+     * <p>
+     * Note that for this method only the direction of <b>b</b> has to be given. The length of <b>b</b> is automatically
+     * derived.
+     *
+     * <p>
+     * A typical use case would be an entity that bounces on a plane. At one point it collides with
+     * the ground, which should invert the downward (vertical) component of its motion. The horizontal motion,
+     * however, should remain the same.
+     *
+     * @param direction the direction in which the speed should be inverted, as a {@code double}
+     */
+    void invertSpeedInDirection(final double direction);
+
+    /**
      * @param direction the {@link Direction} in which the speed of the vector describing the motion, should be
      *                  maximized
      * @param speed     the speed as a {@link double} to which the vector should be maximized
@@ -77,14 +110,15 @@ public interface MotionModifier extends SpeedProvider, DirectionProvider {
     void maximizeMotionInDirection(final double direction, final double speed);
 
     /**
-     * @param direction the {@link Direction} in which the motion should be negated.
+     * @param direction the {@link Direction} in which the speed should be nullified
      * @see #nullifySpeedInDirection(double)
      */
     void nullifySpeedInDirection(final Direction direction);
 
     /**
      * Since the motion can be described as a vector, such a vector can be decomposed in two perpendicular
-     * components. Calling this methods nullifies one of those two components, leaving the other as the new motion.
+     * components, of which one is parallel to the given direction. Calling this methods nullifies that vector,
+     * leaving the other as the new motion.
      *
      * <p>
      * <img width="30%" src="doc-files/negate-motion-vectors.svg" alt="Vector representation of negating the motion in a given direction">
@@ -103,7 +137,7 @@ public interface MotionModifier extends SpeedProvider, DirectionProvider {
      * the ground, which should cancel the vertical downward motion. The vertical motion, however, should not be
      * cancelled. In this case, one should call this method with the parameter {@link Direction#DOWN}.
      *
-     * @param direction the direction in which the motion should be negated, as a {@code double}
+     * @param direction the direction in which the speed should be nullified, as a {@code double}
      */
     void nullifySpeedInDirection(final double direction);
 
