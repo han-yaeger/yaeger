ad3a2caf99f5600786c57bb45f622c1d3a0ef2f9
cassiboy1203 <casvstb@gmail.com>
2021-10-24 10:48:46 +0200

    angle to on coordinate (#216)
    * [bugfix] Resolves #208
    
    * [release] Prepared project for new (bugfix) release
    
    * angle to on coordinate
    
    * deleted duplicate code
    
    * changed version
    
    Co-authored-by: Meron Brouwer <meron.brouwer@gmail.com>

--- src/test/java/com/github/hanyaeger/api/entities/Coordinate2DTest.java
+++ src/test/java/com/github/hanyaeger/api/entities/Coordinate2DTest.java
@@ -1,12 +1,24 @@
 package com.github.hanyaeger.api.entities;
 
 import com.github.hanyaeger.api.Coordinate2D;
+import javafx.geometry.BoundingBox;
 import javafx.geometry.Point2D;
+import javafx.scene.Node;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 class Coordinate2DTest {
+
+    private static final Coordinate2D LOCATION = new Coordinate2D(37, 37);
 
     @Test
     void addingPointWorksAsExpected() {
@@ -34,5 +46,64 @@ class Coordinate2DTest {
         // Assert
         assertEquals(1d, addedPosition.getX());
         assertEquals(1d, addedPosition.getY());
+    }
+
+    @Test
+    void angleToSelfIs0() {
+        // Arrange
+        var expected = 0d;
+
+        // Act
+        var actual = LOCATION.angleTo(LOCATION);
+
+        // Assert
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void angleToOwnCoordinatesIs0() {
+        // Arrange
+        var other = new Coordinate2D(LOCATION.getX(),LOCATION.getY());
+        var expected = 0d;
+
+        // Act
+        var actual = LOCATION.angleTo(new Coordinate2D(other.getX(), other.getY()));
+
+        // Assert
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void angleToNullCoordinate2DThrowsNullPointerException() {
+        // Arrange
+        Coordinate2D other = null;
+
+        // Act & Assert
+        assertThrows(NullPointerException.class, () -> LOCATION.angleTo(other));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForAngleTo")
+    void testAngleToOtherCoordinate(final Coordinate2D otherLocation, final double expectedAngle) {
+
+        // Act
+        var actual = LOCATION.angleTo(otherLocation);
+
+        // Assert
+        assertEquals(expectedAngle, actual);
+    }
+
+    private static Stream<Arguments> provideArgumentsForAngleTo() {
+        return Stream.of(
+                Arguments.of(new Coordinate2D(LOCATION.getX(), LOCATION.getY() - 10), Direction.UP.getValue()),
+                Arguments.of(new Coordinate2D(LOCATION.getX(), LOCATION.getY() + 10), Direction.DOWN.getValue()),
+                Arguments.of(new Coordinate2D(LOCATION.getX() - 10, LOCATION.getY()), Direction.LEFT.getValue()),
+                Arguments.of(new Coordinate2D(LOCATION.getX() + 10, LOCATION.getY()), Direction.RIGHT.getValue()),
+                Arguments.of(new Coordinate2D(LOCATION.getX() - 10, LOCATION.getY() - 10), 225d), // left above
+                Arguments.of(new Coordinate2D(LOCATION.getX() + 10, LOCATION.getY() - 10), 135d), // right above
+                Arguments.of(new Coordinate2D(LOCATION.getX() - 10, LOCATION.getY() + 10), 315d), // left below
+                Arguments.of(new Coordinate2D(LOCATION.getX() + 10, LOCATION.getY() + 10), 45d) // right below
+
+        );
     }
 }
