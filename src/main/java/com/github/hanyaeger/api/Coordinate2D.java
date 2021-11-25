ccbe4ede1f1db41731905e6b8a4395004af3fccd
Wiebe van Aken <wiebevaken@gmail.com>
2021-11-25 11:46:45 +0100

    [Enhancement] Wrote vector subraction, division, multiplication and inversion (#223)
    * Wrote vector subraction, division, multiplication and inversion
    Wrote unit tests
    
    * Added forgotten vector methods
    
    * Added unit tests, removed dotproduct
    
    * Fixed unit tests

--- src/main/java/com/github/hanyaeger/api/Coordinate2D.java
+++ src/main/java/com/github/hanyaeger/api/Coordinate2D.java
@@ -49,7 +49,86 @@ public class Coordinate2D extends Point2D {
     public Coordinate2D add(final Coordinate2D location) {
         return new Coordinate2D(super.add(location));
     }
-
+    
+    /**
+     * Return a {@code Coordinate2D} with the coordinates of the specified point subtracted from the coordinates of this
+     * {@code Coordinate2D}.
+     * 
+     * @param location the {@code Coordinate2D} with the subtracted coordinates
+     * @return the {@code Coordinate2D} with the subtracted coordinates
+     * throws {@link NullPointerException} if the specified {@code Coordinate2D} is null
+     */
+    public Coordinate2D subtract(final Coordinate2D location) {
+    	return new Coordinate2D(super.subtract(location));
+    }
+    
+    /**
+     * Return a {@code Coordinate2D} with the coordinates of the specified point multiplied by the coordinates of this
+     * {@code Coordinate2D}.
+     * 
+     * @param location the {@code Coordinate2D} with the multiplied coordinates
+     * @return the {@code Coordinate2D} with the multiplied coordinates
+     * throws {@link NullPointerException} if the specified {@code Coordinate2D} is null
+     */
+    public Coordinate2D multiply(final Coordinate2D location) throws NullPointerException {
+    	if(location == null) {
+    		throw new NullPointerException("'location' was null");
+    	}
+    	
+    	return new Coordinate2D(this.getX() * location.getX(), this.getY() * location.getY());
+    }
+    
+    /**
+     * Return a {@code Coordinate2D} with the coordinates of the specified point divided by the coordinates of this
+     * {@code Coordinate2D}.
+     * 
+     * @param location the {@code Coordinate2D} with the divided coordinates
+     * @return the {@code Coordinate2D} with the divided coordinates
+     * throws {@link NullPointerException} if the specified {@code Coordinate2D} is null
+     * throws {@link IllegalArgumentException} if an X,Y component of either coordinate is 0
+     */
+    public Coordinate2D divide(final Coordinate2D location) throws NullPointerException, IllegalArgumentException {
+    	if(location == null) {
+    		throw new NullPointerException("'location' was null");
+    	}
+    	
+    	if(this.getX() == 0.D || this.getY() == 0.D || location.getX() == 0.D || location.getY() == 0.D) {
+    		throw new IllegalArgumentException("An X,Y component of either coordinate is 0");
+    	}
+    	
+    	return new Coordinate2D(this.getX() / location.getX(), this.getY() / location.getY());
+    }
+    
+    /**
+     * Returns a {@code Coordinate2D} which lies in the middle between this {@code Coordinate2D} and the specified {@code Coordinate2D}.
+     * @param location is the other endpoint
+     * @return the coordinate in the middle.
+     * throws {@link NullPointerException} if the specified {@code Coordinate2D} is null
+     */
+    public Coordinate2D midPoint(final Coordinate2D location) throws NullPointerException  {
+    	if(location == null) {
+    		throw new NullPointerException("'location' was null");
+    	}
+    	
+    	return new Coordinate2D(super.midpoint(location));
+    }
+    
+    /***
+     * Return a normalized {@code Coordinate2D}.
+     * @return the normalized variant of 'this' vector
+     */
+    public Coordinate2D normalize() {
+    	return new Coordinate2D(super.normalize());
+    }
+    
+    /**
+     * Return a {@code Coordinate2D} which is the inverse of this coordinate
+     * @return the {@code Coordinate2D} with the location of this coordinate, inverted
+     */
+    public Coordinate2D invert() {
+    	return new Coordinate2D(this.getX() * -1, this.getY() * -1);
+    }
+  
     /**
      * Returns a {@code double} with the angle to the specified point of this {@code Coordinate2D}
      * @param coordinate the {@code Coordinate2D} where the angle is going to be calculated to
@@ -70,5 +149,4 @@ public class Coordinate2D extends Point2D {
         }
         return angle;
     }
-
 }
--- src/test/java/com/github/hanyaeger/api/entities/Coordinate2DTest.java
+++ src/test/java/com/github/hanyaeger/api/entities/Coordinate2DTest.java
@@ -47,6 +47,88 @@ class Coordinate2DTest {
         assertEquals(1d, addedPosition.getX());
         assertEquals(1d, addedPosition.getY());
     }
+    
+    @Test
+    void subtractingPointWorksAsExpected() {
+    	// Arrange
+    	var position = new Coordinate2D(0d, 0d);
+    	var positionToSubtract = new Coordinate2D(1d, 1d);
+    	
+    	// Act
+    	var subtractedPosition = position.subtract(positionToSubtract);
+    	
+    	// Assert
+    	assertEquals(-1d, subtractedPosition.getX());
+    	assertEquals(-1d, subtractedPosition.getY());
+    }
+    
+    @Test
+    void subtractingPoint2DWorksAsExpected() {
+    	// Arrange
+    	var position = new Coordinate2D(0d, 0d);
+    	var positionToSubtract = new Point2D(1d, 1d);
+    	
+    	// Act
+    	var subtractedPosition = position.subtract(positionToSubtract);
+    	
+    	// Assert
+    	assertEquals(-1d, subtractedPosition.getX());
+    	assertEquals(-1d, subtractedPosition.getY());
+    }
+    
+    @Test
+    void multiplyPointWorksAsExpected() {
+    	// Arrange
+    	var position = new Coordinate2D(1d, 1d);
+    	var positionToMultiply = new Coordinate2D(2d, 2d);
+    	
+    	// Act
+    	var multipliedPosition = position.multiply(positionToMultiply);
+    	
+    	// Assert
+    	assertEquals(2d, multipliedPosition.getX());
+    	assertEquals(2d, multipliedPosition.getY());
+    }
+    
+    @Test
+    void dividePointWorksAsExpected() {
+    	// Arrange
+    	var position = new Coordinate2D(1d, 1d);
+    	var positionToDivide = new Coordinate2D(2d, 2d);
+    	
+    	// Act
+    	var dividedPosition = position.divide(positionToDivide);
+    	
+    	// Assert
+    	assertEquals(0.5d, dividedPosition.getX());
+    	assertEquals(0.5d, dividedPosition.getY());
+    }
+    
+    @Test
+    void invertPointWorksAsExpected() {
+    	// Arrange
+    	var position = new Coordinate2D(1d, 1d);
+    	
+    	// Act
+    	var invertedPosition = position.invert();
+    	
+    	// Assert
+    	assertEquals(-1d, invertedPosition.getX());
+    	assertEquals(-1d, invertedPosition.getY());
+    }
+    
+    @Test
+    void normalizePointWorksAsExpected() {
+    	// Arrange
+    	var position = new Coordinate2D(1d, 1d);
+    	
+    	// Act
+    	var normalizedPosition = position.normalize();
+    	
+    	// Assert
+    	assertEquals(0.7071067811865475d, normalizedPosition.getX());
+    	assertEquals(0.7071067811865475d, normalizedPosition.getY());
+    }
 
     @Test
     void angleToSelfIs0() {
